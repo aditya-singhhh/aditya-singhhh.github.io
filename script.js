7de4c7961ede0c01f1c2d8184f28d5a98b1f3eef
aditya-singhhh <adityaanurag1969@gmail.com>
2023-10-27 16:00:01 +0530

    Updated Features

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 //typing animation
 var typed = new Typed(".typing",{
-    strings:["Web Developer", "Programmer", "Eletcronics Engineer", "Freelancer"],
+    strings:["Web Developer", "Programmer", "ECE Engineer", "Freelancer"],
     typeSpeed:150,
-    Backspeed:60,
+    Backspeed:40,
     loop:true
 })
+
+var typed2 = new Typed(".typing2",{
+    strings:["Web Developer", "Programmer", "ECE Engineer", "Freelancer"],
+    typeSpeed:150,
+    Backspeed:40,
+    loop:true
+})
+
+
 
 const asideToggler = document.querySelector(".nav-toggler");
 asideToggler.addEventListener("click",() => {
     document.querySelector(".aside").classList.toggle("open");
     document.querySelector(".logo").style.top = "100px";
-    
     })
+
+const listelement = document.querySelector(".nav");
+listelement.addEventListener("click",() =>{
+    document.querySelector(".aside").classList.toggle("open");
+})
